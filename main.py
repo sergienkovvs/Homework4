73ee3a0738eebdf8646ee1c87c55d598058460b3
sergienkovvs <110195868+sergienkovvs@users.noreply.github.com>
2022-08-16 19:07:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-conclusion1 = str(input())
-conclusion2 = list(conclusion1.split(sep=" "))
+conclusion1 = input()
+conclusion2 = conclusion1.split()
 word1 = conclusion2[0].upper()
-word2 = conclusion2[1].capitalize()
+word2 = conclusion2[1].title()
 
-print(word2.split(sep="<<<>>>"))
-print(word1.split(sep="<<<>>>"))
+print(word2.split(), word1.split())
 
 war1 = "!{} {}?".format(word2,word1)
 war2 = "!{0} {1}?".format(word2,word1)
 war3 = "!{a} {b}?".format(a = word2, b = word1)
 
-war1_2 = "!{} {}?".format(word2[::-1],word1[::-1])
-war2_2 = "!{0} {1}?".format(word2[::-1],word1[::-1])
-war3_2 = "!{a} {b}?".format(a = word2[::-1], b = word1[::-1])
+print(war1, war2, war3, sep="<<<>>>")
 
-print(war1.split(sep="<<<>>>"))
-print(war2.split(sep="<<<>>>"))
-print(war3.split(sep="<<<>>>"))
-
-print(war1_2.split(sep="<<<>>>"))
-print(war2_2.split(sep="<<<>>>"))
-print(war3_2.split(sep="<<<>>>"))
-
-file = open("result.txt", "w")
-file.write(war1)
-file.write(war2)
-file.write(war3)
+file = open("result1.txt", "w")
+print(war1, war2, war3, sep="<<<>>>", file=file)
 file.close()
